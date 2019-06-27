24e7e10954b5826d5126facca4129afa3f9f7573
Erin Koen <erin.koen@gmail.com>
2019-06-27 12:07:50 -0400

    This is working for me

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,23 +1,23 @@
 const section = document.querySelectorAll(".bottom");
 console.log(section);
-// TweenMax.from(section, 2, {x: 600});
+TweenMax.from(section, 200, {x: 1200});
 
-// class dropDown {
-//     constructor(element) {
-//         this.element = element;
+class dropDown {
+    constructor(element) {
+        this.element = element;
 
-//         this.button = this.element.querySelector(REPLACEwithBUTTON);
+        this.button = this.element.querySelector(REPLACEwithBUTTON);
 
-//         this.content = document.querySelector(REPLACEwithCONTENT);
+        this.content = document.querySelector(REPLACEwithCONTENT);
 
-//         this.button.addEventListener('click', () => {
-//             this.toggle();
-//           })
-//     }
+        this.button.addEventListener('click', () => {
+            this.toggle();
+          })
+    }
     
-//     toggle(){
-//         this.content.classList.toggle(DROPclassHERE);
-//     }
-// }
+    toggle(){
+        this.content.classList.toggle(DROPclassHERE);
+    }
+}
 
-// let dropdown = document.querySelectorAll().forEach( dropdown => new dropDown(dropdown));
+let dropdown = document.querySelectorAll().forEach( dropdown => new dropDown(dropdown));
